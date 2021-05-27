fd2943ab4d6823cffc67f3d72f9656295450886e
roryb <rbarden1@sheffield.ac.uk>
2021-05-27 16:09:58 +0100

    minor changes

--- RamblersState.java
+++ RamblersState.java
@@ -3,12 +3,19 @@ import java.util.List;
 
 public class RamblersState extends SearchState {
 
-    private int[][] tile;
+    private int[][] state;
 
+    /* goalPredicate checks if a node is a goal node, and returns a boolean for the result.
+    I call the getSuccessors method which I have implemented below. If there are no successors, the
+    node must be a goal node, which I check with isEmpty().
+     */
     boolean goalPredicate(Search searcher) {
         return (getSuccessors(searcher).isEmpty());
     }
 
+    /* getSuccessors iterates through all of the successor nodes in the Search object and adds their
+    state to a list which is then returned
+     */
     ArrayList<SearchState> getSuccessors(Search searcher) {
         ArrayList<SearchState> successorList = new ArrayList<>();
         for (SearchNode node : searcher.successorNodes) {
@@ -17,7 +24,9 @@ public class RamblersState extends SearchState {
         return successorList;
     }
 
+    /* checks if the superclass state is equal to the provided state
+     */
     boolean sameState(SearchState n2) {
-        n2.
+        return (super.toString().equals(n2.toString()));
     }
 }
--- RunRamblersBB.java
+++ RunRamblersBB.java
@@ -1,8 +1,14 @@
 import java.util.Random;
-
+// Branch and bound is default strategy if depth or breadth first not specified in strat
 public class RunRamblersBB {
     static Random rand = new Random();
     public static void main(String[] args) {
-
+        TerrainMap tm = new TerrainMap("tmc.pgm");
+        for (int i = 0; i < 50; i++) {
+            // Create a new random coordinate based on map dimensions for start and goal
+            Coords start = new Coords((rand.nextInt(tm.getDepth())), (rand.nextInt(tm.getHeight())));
+            Coords goal = new Coords((rand.nextInt(tm.getDepth())), (rand.nextInt(tm.getHeight())));
+            Search bandb = new Search.runSearch();
+        }
     }
 }
